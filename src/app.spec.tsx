87cd7a9715f16e09695fad519811f3ca93db9d4e
Ícaro Campos <icarocampos97@gmail.com>
2021-10-16 09:49:31 -0300

    ✅ add teste snapshop

--- src/app.spec.tsx
+++ src/app.spec.tsx
@@ -1,8 +1,19 @@
-import { render } from "@testing-library/react";
-import { App } from "./App"
+import { render, screen, waitFor } from "@testing-library/react";
+import { App } from "./App";
+import  renderer from 'react-test-renderer';
 
 describe('App', () => {
   it('deve renderizar o componente', () => {
+    const tree = renderer
+    .create(<App/>)
+    .toJSON()
+    expect(tree).toMatchSnapshot()
     render(<App />)
   })
+
+  it('deve renderizar a lista', async () => {
+    render(<App />)
+    await waitFor (() => screen.getByText('Olimpiadas App'))
+  })
 })
+
